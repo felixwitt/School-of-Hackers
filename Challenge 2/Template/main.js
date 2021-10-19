df9eb0cc64b77553c6b638e167b9a360c41e66cd
felhol <felix.holfelder@witt-gruppe.eu>
2021-10-19 07:37:39 +0200

    some changes

--- Challenge 2/Template/main.js
+++ Challenge 2/Template/main.js
@@ -7,13 +7,46 @@ function klick(id) {
         document.getElementById(id).disabled = true
         checkFinish()
     } else {
-
+        currentPlayer = 'X'
+        document.getElementById(id).innerHTML = 'X'
+        document.getElementById(id).disabled = true
+        checkFinish()
     }
 }
 
 function checkFinish() {
-    if (getElementById("b1").innerHTML === 'X' && getElementById("b2").innerHTML === 'X' && getElementById("b3").innerHTML === 'X') {
-        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! ' + winner + ' hat gewonnen!'
+    if (document.getElementById("b1").innerHTML === 'X' && document.getElementById("b2").innerHTML === 'X' && document.getElementById("b3").innerHTML === 'X') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! X hat gewonnen!'
+    } else if (document.getElementById("b4").innerHTML === 'X' && document.getElementById("b5").innerHTML === 'X' && document.getElementById("b6").innerHTML === 'X') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! X hat gewonnen!'
+    } else if (document.getElementById("b7").innerHTML === 'X' && document.getElementById("b8").innerHTML === 'X' && document.getElementById("b9").innerHTML === 'X') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! X hat gewonnen!'
+    } else if (document.getElementById("b1").innerHTML === 'X' && document.getElementById("b4").innerHTML === 'X' && document.getElementById("b7").innerHTML === 'X') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! X hat gewonnen!'
+    } else if (document.getElementById("b2").innerHTML === 'X' && document.getElementById("b5").innerHTML === 'X' && document.getElementById("b8").innerHTML === 'X') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! X hat gewonnen!'
+    } else if (document.getElementById("b3").innerHTML === 'X' && document.getElementById("b6").innerHTML === 'X' && document.getElementById("b9").innerHTML === 'X') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! X hat gewonnen!'
+    } else if (document.getElementById("b1").innerHTML === 'X' && document.getElementById("b5").innerHTML === 'X' && document.getElementById("b9").innerHTML === 'X') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! X hat gewonnen!'
+    } else if (document.getElementById("b3").innerHTML === 'X' && document.getElementById("b5").innerHTML === 'X' && document.getElementById("b7").innerHTML === 'X') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! X hat gewonnen!'
+    } else if (document.getElementById("b1").innerHTML === 'O' && document.getElementById("b2").innerHTML === 'O' && document.getElementById("b3").innerHTML === 'O') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! O hat gewonnen!'
+    } else if (document.getElementById("b4").innerHTML === 'O' && document.getElementById("b5").innerHTML === 'O' && document.getElementById("b6").innerHTML === 'O') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! O hat gewonnen!'
+    } else if (document.getElementById("b7").innerHTML === 'O' && document.getElementById("b8").innerHTML === 'O' && document.getElementById("b9").innerHTML === 'O') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! O hat gewonnen!'
+    } else if (document.getElementById("b1").innerHTML === 'O' && document.getElementById("b4").innerHTML === 'O' && document.getElementById("b7").innerHTML === 'O') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! O hat gewonnen!'
+    } else if (document.getElementById("b2").innerHTML === 'O' && document.getElementById("b5").innerHTML === 'O' && document.getElementById("b8").innerHTML === 'X') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! O hat gewonnen!'
+    } else if (document.getElementById("b3").innerHTML === 'O' && document.getElementById("b6").innerHTML === 'O' && document.getElementById("b9").innerHTML === 'O') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! O hat gewonnen!'
+    } else if (document.getElementById("b1").innerHTML === 'O' && document.getElementById("b5").innerHTML === 'O' && document.getElementById("b9").innerHTML === 'O') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! O hat gewonnen!'
+    } else if (document.getElementById("b3").innerHTML === 'O' && document.getElementById("b5").innerHTML === 'O' && document.getElementById("b7").innerHTML === 'O') {
+        document.getElementById("result").innerHTML = 'Das Spiel ist vorbei! O hat gewonnen!'
     }
 }
 
